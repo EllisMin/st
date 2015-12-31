39d803aa53d3049470b688e8d9bd716b0b8f5d24
EllisMin <dbmin@hotmail.com>
2015-12-30 22:44:16 -0600

    login and register pages s created

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -9,21 +9,51 @@
 package com.parse.starter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.parse.ParseAnalytics;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 
 public class MainActivity extends Activity {
+  EditText usernameField;
+  EditText passwordField;
 
+
+
+  // When click Register button
+  public void registerBtn(View view){
+
+    Log.i("AA", String.valueOf(passwordField.getText()));
+    
+    // Shows up the Register activity
+    Intent i = new Intent(getApplicationContext(), Register.class);
+    startActivity(i);
+  }
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    // Textfield setting
+    usernameField = (EditText) findViewById(R.id.username);
+    passwordField = (EditText) findViewById(R.id.password);
+
+    // If the user is logged in, go to Search (main) page
+//    if(ParseUser.getCurrentUser() != null){
+//      Intent i = new Intent(getApplicationContext(), CLASSNAME);
+//      startActivity(i);
+//    }
+
 
     ParseObject score = new ParseObject("Score");
     score.put("username", "rob");
---
+++ ParseStarterProject/src/main/java/com/parse/starter/Register.java
@@ -0,0 +1,25 @@
+package com.parse.starter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Register extends AppCompatActivity {
+
+
+    // When hit back button
+    public void back_reg(View view){
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
