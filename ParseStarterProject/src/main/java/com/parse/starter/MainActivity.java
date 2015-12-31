a42a173f81a04340394dc1b1810df13d8a3c2f10
EllisMin <dbmin@hotmail.com>
2015-12-31 00:36:59 -0600

    login function added

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -17,24 +17,48 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.LogInCallback;
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
   EditText usernameField;
   EditText passwordField;
 
+  TextView loginBtn;
 
+  // Method to show Search view
+  public void showSearch(){
+    Intent i = new Intent(getApplicationContext(), Search.class);
+    startActivity(i);
+  }
+  // When anyting that has OnClickListener is clicked (in this case, login button)
+  public void onClick(View v) {
+    if(v.getId() == R.id.loginBtn){
+      ParseUser.logInInBackground(String.valueOf(usernameField.getText()), String.valueOf(passwordField.getText()), new LogInCallback() {
+        @Override
+        public void done(ParseUser user, ParseException e) {
+
+          if (user != null) {
+            Log.i("AppInfo", "Login Successful");
+            showSearch();
+          }
+          else
+            Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_LONG).show();
+        }
+      });
+    }
+  }
 
   // When click Register button
   public void registerBtn(View view){
 
-    Log.i("AA", String.valueOf(passwordField.getText()));
-    
     // Shows up the Register activity
     Intent i = new Intent(getApplicationContext(), Register.class);
     startActivity(i);
@@ -44,24 +68,22 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
+    // If the user is logged in, go to Search (main) page
+    if (ParseUser.getCurrentUser() != null){
+      showSearch();
+    }
+
     // Textfield setting
     usernameField = (EditText) findViewById(R.id.username);
     passwordField = (EditText) findViewById(R.id.password);
+    loginBtn = (TextView) findViewById(R.id.loginBtn);
 
-    // If the user is logged in, go to Search (main) page
-//    if(ParseUser.getCurrentUser() != null){
-//      Intent i = new Intent(getApplicationContext(), CLASSNAME);
-//      startActivity(i);
-//    }
-
-
-    ParseObject score = new ParseObject("Score");
-    score.put("username", "rob");
-    score.put("score", 96);
-    score.saveInBackground();
+    // Setting textView clickable
+    loginBtn.setOnClickListener(this);
 
 
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
   }
+
 
 }
--- ParseStarterProject/src/main/java/com/parse/starter/Search.java
+++ ParseStarterProject/src/main/java/com/parse/starter/Search.java
@@ -1,29 +1,33 @@
 package com.parse.starter;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
+import com.parse.ParseUser;
+
 public class Search extends AppCompatActivity {
+
+    // Temporary logout button
+    public void tempLogOut(View view){
+        Log.i("AppINFO", "btn clicked!");
+        // Logout
+        ParseUser.logOut();
+        // Show login Page
+        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(i);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
